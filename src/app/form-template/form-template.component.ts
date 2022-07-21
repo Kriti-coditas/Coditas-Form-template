3cdcdb59645ed4865c8ce108c1dea154d2b71329
Kriti-coditas <kriti.singh@coditas.com>
2022-07-21 17:08:05 +0530

    date:21 Commit-2

--- src/app/form-template/form-template.component.ts
+++ src/app/form-template/form-template.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-form-template',
@@ -9,6 +9,8 @@ export class FormTemplateComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) { }
   candidateName!: string;
   candidateDesignation!: string;
+  profileSummaryInPara: string = "[Candidate’s name] is a passionate [primary skill] and a [secondary skill] enthusiast having gained experience in both '[primary domain]' and [secondary domain] overall span of [number] years. He has pursued his [name of degree/diploma] in [subject of specialization] and is currently working as a [designation] at Coditas.";
+  errorMessage: string = "";
   placeHolderForProfileSummary: string = "[Candidate’s name] is a passionate [primary skill] and a [secondary skill] enthusiast having gained experience in both '[primary domain]' and [secondary domain] overall span of [number] years. He has pursued his [name of degree/diploma] in [subject of specialization] and is currently working as a [designation] at Coditas.";
   profileForm!: FormGroup;
   ngOnInit() {
@@ -41,11 +43,16 @@ export class FormTemplateComponent implements OnInit {
     });
   }
   onPrint(profileForm: FormGroup) {
-    // console.log("print button clicked",profileForm);
-    // console.log("value of fullName",profileForm.value.fullName);
-    // this.candidateName=profileForm.value.fullName;
-    // this.candidateDesignation=profileForm.value.profession;
+
     window.print();
+  }
+  checkRequiredFields() {
+
+    if (!this.profileForm.value.fullName || !this.profileForm.value.profession || !this.profileForm.value.profileSummary) {
+      this.errorMessage = "Please fill all the Mandatory fields to Print"
+    } else {
+      this.errorMessage = "";
+    }
   }
 }
 
